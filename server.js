8b4f31fd7eb7b31095420ad7963faea46ff66e59
kaynye <kemuel.kany@live.fr>
2018-06-24 22:26:55 +0200

    ajout du parcour V1

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ var stringify = require('node-stringify');
 var express = require('express');
 
 var app = express();
+app.use('/static', express.static(__dirname + '/public'));
+app.use( express.static( "static" ) );
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 //<liste=getTree();
-liste=[];
+
 //console.log(liste);
 
 app.get('/', function(req, res) {
-	getTree();
-	if (liste!=null){
+	//getTree();
+	//if (liste!=null){
 		res.render('home.ejs');
-	}
+	//}
 });
 
 /* FS.read("hello.txt", //lire
@@ -32,27 +34,47 @@ app.get('/', function(req, res) {
     // expect(hello).toBe("Hello, World!\n")
 // });
 
-function getTree(root){
- FS.list(root).then(function (hello) {//tree
-    //console.log(stringify(hello));
-	liste=hello;
-})
-}
-
 
 
 io.sockets.on('connection', function (socket) {
 	socket.root=".";
+	function getTree(root){
+	 FS.list(root).then(function (hello) {//tree
+		//console.log(stringify(hello));
+		console.log("alohaa");
+		socket.liste=hello;
+		console.log(hello);
+		console.log(socket.liste);
+	});
+	}
+	
+	
 	getTree(socket.root);
-	currentFold=liste;
 	
 	socket.on("Connect",function(){
-		for (file in liste){
+		getListDoc(socket.liste);
+	});
+	
+	socket.on('openFold',function(foldName){
+		goTo(socket.root+'/'+foldName);
+	})
+	
+	function goTo(root){
+		socket.root=root;
+		getTree(socket.root);
+		console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		console.log('goTo='+socket.liste);
+		socket.emit("clear");
+		getListDoc(socket.liste);
+	}
+	
+	function getListDoc(liste){
+		console.log('root'+socket.root);
+		console.log(liste);
+		for (file in socket.liste){
 			//console.log(socket.root+'/'+liste[file]);
 			dire(socket.root+"/"+liste[file],liste[file])
-
 		}
-		
 		function dire(root,nom){
 			console.log("root="+root)
 		 FS.isDirectory(root).then(function (hello) {//tree
@@ -62,11 +84,7 @@ io.sockets.on('connection', function (socket) {
 			console.log("rep="+hello)
 		})
 		}
-		
-	});
-	
-	
-	
+	}
 });
 
 server.listen(8080);
